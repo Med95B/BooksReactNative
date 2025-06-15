2117f4599266401fd5e8d626647152eb4b0caf74
Med95B <bouizloufa@gmail.com>
2025-06-15 15:28:43 +0100

    books routes done

--- backend/src/routes/booksRoutes.js
+++ backend/src/routes/booksRoutes.js
@@ -35,4 +35,74 @@ router.post('/',protectRoute,async(req,res)=>{
         }
 })
 
+router.get('/',protectRoute,async(req,res)=>{
+
+    try {
+        const page=req.query.page || 1
+        const limit=req.query.limit || 5
+        const skip=(page - 1) * limit
+
+        const books=await Book.find()
+        .sort({createdAt:-1})
+        .skip(skip)
+        .limit(limit)
+        .populate('user','username profil')
+       
+        const totalBooks=await Book.countDocuments()
+       
+        res.send({
+            books,
+            currentPage:page,
+            totalBooks,
+            totalPages:Math.ceil(totalBooks/limit)
+        })
+
+    } catch (error) {
+        console.log('error geting books',error);
+        res.status(500).json({message:'server error'})
+    }
+})
+
+router.delete('/:id',protectRoute,async(req,res)=>{
+
+    try {
+        const book = await Book.findById(req.params.id)
+        if (!book) {
+            return res.status(404).json({message:'book not fund'})
+        }
+
+        if (book.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({message: 'unauthorized'})
+        }
+
+        if (book.image && book.image.includes('cloudinary')) {
+            try {
+                const publicId=book.image.split('/').pop().split('.')[0]
+                await cloudinary.uploader.destroy(publicId)
+
+            } catch (deleteError) {
+                console.log('error deleting image from cloudinary',deleteError);
+                
+            }
+        }
+        await book.deleteOne()
+
+        res.json({message: 'book deleted successfully'})
+    } catch (error) {
+        console.log('error deleting book',error);
+    res.status(500).json({message:'server error'})        
+    }
+})
+
+
+router.get('/user',protectRoute,async (req,res) => {
+    try {
+        const books=(await Book.find({user:req.user._id})).sort({createdAt:-1})
+        res.send(books)
+    } catch (error) {
+        console.log('get user books error',error.message);
+        res.status(500).json({message:'server error'})
+        
+    }
+})
 export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import 'dotenv/config' 
+import cors from 'cors'
 import authRoutes from './routes/authRoutes.js'
 import booksRoutes from './routes/booksRoutes.js'
 import { connectDB } from './lib/db.js'
 
+
 const app=express()
 const PORT=process.env.PORT
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth',authRoutes)
 app.use('/api/books',booksRoutes)
